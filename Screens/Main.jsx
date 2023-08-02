864ad6ab5614ab23ec16de98b4e9d3d7de088485
Sean Winnik <winnik.sean@gmail.com>
2023-08-02 10:20:42 -0700

    building out main page NOT schema

--- Screens/Main.jsx
+++ Screens/Main.jsx
@@ -9,6 +9,12 @@ const MainScreen = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
+      <View style={styles.barChart}>
+        <Text> Bar Chart Attendance</Text>
+      </View>
+      <View style={styles.icon}>
+        <Text> Academy Logo</Text>
+      </View>
       <Text>Sup tyler!</Text>
       <Button
         title="NewEntry"
@@ -24,6 +30,29 @@ const MainScreen = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
+  icon: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    height: 120,
+    width: "30%",
+    backgroundColor: "brown",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  barChart: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    height: 120,
+    width: "70%",
+    backgroundColor: "purple",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
   container: {
     flex: 1,
     backgroundColor: "#fff",
