9dc83326117840893e08b3dbcd3f49f02976ec8d
Sean Winnik <winnik.sean@gmail.com>
2023-08-04 10:19:39 -0700

    SOME junk data published

--- index.ts
+++ index.ts
@@ -2,27 +2,34 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function main() {
-  await prisma.user.create({
-    data: {
-      name: "Sean",
-      email: "Sean@prisma.io",
-      posts: {
-        create: { title: "Hello World" },
-      },
-      profile: {
-        create: { bio: "I like turtles" },
-      },
-    },
-  });
+// async function main() {
+//   //   await prisma.user.create({
+//   //     data: {
+//   //       name: "Gertrude",
+//   //       email: "Gertrude@prisma.io",
+//   //       posts: {
+//   //         create: { title: "Hello World" },
+//   //       },
+//   //       profile: {
+//   //         create: { bio: "I like turtles" },
+//   //       },
+//   //     },
+//   //   });
 
-  const allUsers = await prisma.user.findMany({
-    include: {
-      posts: true,
-      profile: true,
-    },
+//   const allUsers = await prisma.user.findMany({
+//     include: {
+//       posts: true,
+//       profile: true,
+//     },
+//   });
+//   console.dir(allUsers, { depth: null });
+// }
+async function main() {
+  const post = await prisma.user.update({
+    where: { id: 4 },
+    data: { published: true },
   });
-  console.dir(allUsers, { depth: null });
+  console.log(post);
 }
 
 main()
